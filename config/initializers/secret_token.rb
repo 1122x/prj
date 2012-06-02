ddc0d671334e86da3695378fdf6810ecbfd668e8
Joseph Pawletko <jgpawletko@gmail.com>
2012-06-02 14:36:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prj::Application.config.secret_token = '0b1ac625ade56865f410346a98875e0fdd60e1e698922489749a51d81c49f60b65557fcd5d6a5379ba6a9eded6958a8d945f1ec288bc842079e93dbd347548c4'
